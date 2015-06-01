cbbeca78881beef8fac7adebf6ec912dcda55cde
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2015-06-01 00:32:33 -0300

    create fake home layout

---
+++ server-fake/generate-server.js
@@ -0,0 +1,28 @@
+module.exports = function() {
+	var faker = require('faker');
+	var _ = require('lodash');
+
+	return {
+		users: _.times(2, function(n) {
+			return {
+				id: _.uniqueId(),
+				name: faker.name.findName(),
+				email: faker.internet.email()
+			}
+		}),
+		banners: _.times(3, function(n) {
+			return {
+				id: n,
+				imagem: faker.image.imageUrl(980, 300)
+			}
+		}),
+		products: _.times(6, function(n) {
+			return {
+				id: n,
+				imagem: faker.image.imageUrl(200, 200),
+				description: faker.lorem.sentence(),
+				price: Math.floor(Math.random() * 1000)
+			}
+		})
+	}
+}
